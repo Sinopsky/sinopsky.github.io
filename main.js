73b365f17bbf95d4265f961db4328941a53414ef
Sinopsky <79845165+Sinopsky@users.noreply.github.com>
2023-12-10 16:52:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ let composer = new EffectComposer( renderer );
 				composer.addPass( ssaaRenderPassP );
                 const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
                 bloomPass.threshold = 0.2;
-	            bloomPass.strength = 0.4;
+	            bloomPass.strength = 1;
 	            bloomPass.radius = 1;
                 composer.addPass( bloomPass );
 				const outputPass = new OutputPass();
